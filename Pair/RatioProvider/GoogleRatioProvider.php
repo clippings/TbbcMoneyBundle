766a2d653d8b27eba83539de6ea880201919d6c9
enekochan <enekochan@gmail.com>
2018-05-24 19:49:26 +0200

    Make services public so they can be obtained from the container in Symfony 4 and beyond (#94)
    * Make services public so they can be obtained from the container in Symfony 4 and beyond
    
    * Update Google Finance Currency Converter URL

--- Pair/RatioProvider/GoogleRatioProvider.php
+++ Pair/RatioProvider/GoogleRatioProvider.php
@@ -18,6 +18,52 @@ class GoogleRatioProvider implements RatioProviderInterface
      */
     public function fetchRatio($referenceCurrencyCode, $currencyCode)
     {
+        $isoCurrencies = new ISOCurrencies();
+
+        $baseCurrency = new Currency($referenceCurrencyCode);
+        if (!$baseCurrency->isAvailableWithin($isoCurrencies)) {
+            throw new MoneyException(
+                sprintf('The currency code %s does not exists', $referenceCurrencyCode)
+            );
+        }
+
+        $currency = new Currency($currencyCode);
+        if (!$currency->isAvailableWithin($isoCurrencies)) {
+            throw new MoneyException(
+                sprintf('The currency code %s does not exists', $currencyCode)
+            );
+        }
+
+        $baseUnits = 1000;
+        $endpoint = $this->getEndpoint($baseUnits, $baseCurrency, $currency);
+        $responseString = file_get_contents($endpoint);
+        $convertedAmount = $this->getConvertedAmountFromResponse($responseString);
+        $ratio = $convertedAmount / $baseUnits;
+
+        return $ratio;
+    }
+
+    /**
+     * @param string   $units
+     * @param Currency $referenceCurrency
+     * @param Currency $currency
+     * @return string The endpoint to get Currency conversion
+     */
+    protected function getEndpoint($units, Currency $referenceCurrency, Currency $currency)
+    {
+        return sprintf(
+            'https://finance.google.com/bctzjpnsun/converter?a=%s&from=%s&to=%s',
+            $units,
+            $referenceCurrency->getCode(),
+            $currency->getCode()
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function fetchRatio($referenceCurrencyCode, $currencyCode)
+    {
         $adapter = new SwapAdapterRatioProvider(
             new GoogleFinanceProvider(new CurlHttpAdapter())
         );
